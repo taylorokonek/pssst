747d1f02821ca2f20b322adb0980aa8036374fb9
Taylor Okonek <43622297+taylorokonek@users.noreply.github.com>
2023-02-03 14:32:01 -0800

    allow for right-censoring time (end of study) in format_dhs

--- R/format_dhs.R
+++ R/format_dhs.R
@@ -9,6 +9,9 @@
 #' enter the latest one
 #' @param period_boundaries a numeric vector containing the years surrounding each time period of interest.
 #' Example: if periods are 2000-2005 and 2005-2010, \code{period_boundaries} should be \code{c(2000,2005,2010)}.
+#' @param right_censor_time age in months at which every individual who was not yet died
+#' should be right-censored. If there is no such age, set equal to NA. If estimating U5MR, 
+#' consider setting to 60.
 #' @param strata a string vector containing which column in \code{df} contains the strata 
 #' information. Can be multiple columns. Defaults to "v023".
 #' @return A dataframe containing the births recode in a format that can be input to \code{surv_synthetic}.
@@ -18,6 +21,7 @@
 format_dhs <- function(df, 
                        survey_year, 
                        period_boundaries, 
+                       right_censor_time = NA,
                        strata = c("v023", "v024", "v025")[1]) {
   
   # get year_cut from period_boundaries
@@ -30,6 +34,25 @@ format_dhs <- function(df,
   num_before <- length(which(births$year_born < min(year_cut)))
   message(paste0("Removing ", num_before, " children born before ", min(year_cut)))
   births <- births %>% filter(year_born >= min(year_cut))
+  
+  # if a right_censor_time is specified, do that
+  if (!is.na(right_censor_time)) {
+    # identify who dies after right_censor_time
+    row_ids <- which(births$died & (births$age_at_censoring > right_censor_time))
+    
+    # instead make them right censored
+    births[row_ids,]$right_censored <- TRUE
+    births[row_ids,]$interval_censored <- FALSE
+    births[row_ids,]$died <- FALSE
+    births[row_ids,]$age_at_censoring <- right_censor_time
+    births[row_ids,]$t0 <- 0
+    births[row_ids,]$t1 <- Inf
+    
+    # identify who is right censored later than right_censor_time
+    row_ids <- which(!births$died & (births$age_at_censoring > right_censor_time))
+    births[row_ids,]$age_at_censoring <- right_censor_time
+  }
+  
   
   # expand dataframe
   births <- df_expand(surv_df = births, 
