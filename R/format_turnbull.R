3ff92bdce3b54e27d4a00751439b7f7f9cf11abf
Taylor Okonek <43622297+taylorokonek@users.noreply.github.com>
2023-02-04 20:07:31 -0800

    bug fixes in turnbull & add in jackknife variance option

--- R/format_turnbull.R
+++ R/format_turnbull.R
@@ -22,34 +22,85 @@ format_turnbull <- function(df) {
   I1_df$lefttrunc <- NA
   I1_df$righttrunc <- NA
   
+  # set up dataframe
+  ret_small_0 <- I0_df[1,]
+  
   for (i in 1:length(unique(I0_df$individual))) {
-    row_ids <- which(I0_df$individual == I0_df$individual[i])
+    row_ids <- which(I0_df$individual == unique(I0_df$individual)[i])
     test <- I0_df[row_ids,]
     test <- test %>%
       dplyr::mutate(lefttrunc = ifelse(a_pi < 0, 0, a_pi),
              righttrunc = Inf,
              t_0i = lefttrunc)
     test$t_1i <- c(test$lefttrunc[-1],test$t_i[1])
-    I0_df[row_ids,] <- test
+    
+    # get rows where they are alive
+    test_small <- test[(test$a_pi >= -60) & (test$a_pi < test$t_i[1]),]
+    
+    # for turnbull() function, t_0i needs to be censoring time, t_1i needs to be Inf 
+    test_small$t_1i <- ifelse(test_small$t_1i > test_small$t_i[1], test_small$t_i[1], test_small$t_1i)
+    test_small$t_0i <- test_small$t_1i
+    test_small$t_1i <- Inf
+    
+    # rbind
+    ret_small_0 <- rbind(ret_small_0, test_small)
   }
   
-  I0_df_small <- I0_df %>% dplyr::filter(a_pi >= -60) 
+  # set up dataframe
+  ret_small <- I1_df[1,]
   
   for (i in 1:length(unique(I1_df$individual))) {
     row_ids <- which(I1_df$individual == unique(I1_df$individual)[i])
     test <- I1_df[row_ids,]
-    test <- test %>%
-      dplyr::mutate(lefttrunc = ifelse(a_pi < 0, 0, a_pi),
-             righttrunc = Inf,
-             t_0i = pmin(t_0i,lefttrunc))
-    test$t_1i <- pmin(c(test$lefttrunc[-1],test$t_1i[1]), test$t_1i[1])
-    I1_df[row_ids,] <- test
+    
+    # if not interval censored across time period boundary
+    if (test$A_i[1] == 0) {
+      # which time period are they interval censored in
+      which_cens <- tail(which(test$t_0i >= test$a_pi), 1)
+      
+      # which time periods are they alive in
+      which_alive <- which((test$t_0i >= test$a_pi) & (test$t_1i <= (test$a_pi + 60)))
+      
+      # if they're only alive in one period...
+      if (length(which_alive == 1)) {
+        test_small <- test[which_cens,]
+        test_small$lefttrunc <- 0
+        test_small$righttrunc <- Inf
+        
+        # rbind
+        ret_small <- rbind(ret_small, test_small)
+      } else {
+        print(i)
+        stop("function not built to handle yearly time periods yet, where individuals\ncan be alive through multiple time periods before being interval censored")
+      }
+    
+    # if interval censored across time period boundary
+    } else {
+      # which time periods are they interval censored in
+      which_cens <- which((test$t_0i <= (test$a_pi + 60)) & (test$t_1i > test$a_pi))
+      
+      if (length(which_cens) != 2) {
+        stop("function not built to handle people interval censored across three time periods yet")
+      }
+      
+      test_small <- test[which_cens,]
+      test_small$t_0i <- c(test_small$t_0i[1], test_small$a_pi[2])
+      test_small$t_1i <- c(test_small$a_pi[2], test_small$t_1i[1])
+      test_small$lefttrunc <- c(0, test_small$a_pi[2])
+      test_small$righttrunc <- Inf
+      
+      # rbind
+      ret_small <- rbind(ret_small, test_small)
+    }
+    
+
   }
   
-  I1_df_small <- I1_df %>% 
-    dplyr::filter(lefttrunc < t_1i)
+  # remove first row of ret_small
+  ret_small_0 <- ret_small_0[-1,]
+  ret_small <- ret_small[-1,]
   
-  turnbull_df <- rbind(I0_df_small, I1_df_small)
+  turnbull_df <- rbind(ret_small_0, ret_small)
   
   return(turnbull_df)
 }
--- R/turnbull.R
+++ R/turnbull.R
@@ -11,7 +11,13 @@
 #' @param righttrunc column corresponding to right-truncation time \code{df}, \code{Inf} if not
 #' right-truncated
 #' @param weights column corresponding to weights in \code{df}
+#' @param jackknife_var indicator for whether or not to compute jackknife variance of the estimator.
+#' Will approximate the finite sample variance when sample size is small compared to population size. Defaults
+#' to \code{FALSE}. If \code{jackknife_var} = \code{TRUE}, then both cluster and strata must be specified
+#' @param cluster column corresponding to cluster ID from survey design. Only used if \code{jackknife_var} = \code{TRUE}. 
+#' @param strata column corresponding to strata ID from survey design. Only used if \code{jackknife_var} = \code{TRUE}. 
 #' @param niter number of iterations to run the algorithm
+#' @param niter_jackknife number of iterations to run each resample. Defaults to \code{niter}
 #' @return a list containing the following:
 #' \itemize{
 #' \item a dataframe of final estimates for specific times
@@ -27,8 +33,22 @@ turnbull <- function(df,
                      lefttrunc = "lefttrunc", 
                      righttrunc = "righttrunc", 
                      weights = "weights", 
-                     niter) {
+                     jackknife_var = FALSE,
+                     cluster = NA,
+                     strata = NA,
+                     niter,
+                     niter_jackknife = niter) {
+  
   # error checking - TO DO
+  if (jackknife_var) {
+    if (is.na(cluster) | is.na(strata)) {
+      stop("Must specify cluster and strata if jackknife_var = TRUE")
+    }
+  }
+  
+  # make cluster and strata factors
+  df$cluster <- factor(df[,cluster])
+  df$strata <- factor(df[,strata])
   
   # call rcpp_turnbull
   res <- rcpp_turnbull(niter = niter, 
@@ -38,13 +58,84 @@ turnbull <- function(df,
                        righttrunc = df[,righttrunc], 
                        weights = df[,weights]) 
   
-  # create final data frame
-  res_df <- data.frame(t0 = res[[1]],
-             t1 = res[[2]],
-             est = 1 - cumsum(res[[3]][,niter]))
+  # calculate jackknife variance
+  if (jackknife_var) {
+    message("Calculating jackknife variance...")
+    
+    # get data frame containing unique clusters and stratas
+    c_s_df <- df[,c(cluster, strata)] %>% dplyr::distinct()
+    
+    # number of strata
+    h_df <- df %>% 
+      dplyr::group_by(strata) %>% 
+      dplyr::summarise(n_h = length(unique(cluster))) %>% 
+      as.data.frame()
+    h <- nrow(h_df)
+    
+    # number of clusters in each strata
+    n_h <- h_df[,2]
+    
+    # number of clusters
+    k <- length(unique(df[,cluster]))
+    
+    # calculate h_k
+    h_k <- (n_h - 1)/n_h
+    h_k <- rep(h_k, n_h)
+    
+    # loop through clusters
+    r_i <- matrix(NA, nrow = nrow(res[[3]]), ncol = k)
+    message(paste0("Looping through ", k, " clusters..."))
+    for (i in 1:k) {
+      # filter dataset to exclude cluster k
+      tmp <- df[df[,cluster] != unique(df[,cluster])[i],]
+      
+      # adjust weights of other clusters in stratum
+      which_strata <- c_s_df[,strata][c_s_df[,cluster] == i]
+      other_clusts <- c_s_df[,cluster][c_s_df[,strata] == which_strata]
+      other_clusts <- other_clusts[other_clusts != unique(df[,cluster])[i]]
+      
+      tmp[tmp[,cluster] %in% other_clusts, weights] <- n_h[which_strata]/ (n_h[which_strata] - 1)
+      
+      # get turnbull estimator on tiny dataset
+      small_samp_est <- rcpp_turnbull(niter = niter_jackknife, 
+                                      t0 = tmp[,t_0i],
+                                      t1 = tmp[,t_1i],
+                                      lefttrunc = tmp[,lefttrunc], 
+                                      righttrunc = tmp[,righttrunc],
+                                      weights = tmp[,weights])
+      
+      # add results to r_i
+      r_i[,i] <- small_samp_est[[3]][,niter_jackknife]
+    }
+    
+    # calculate variance
+    r_i_minus_r_2 <- apply(r_i, 2, function(x) {(x - res[[3]][,niter])^2})
+    for (i in 1:ncol(r_i)) {
+      r_i_minus_r_2[,i] <- r_i_minus_r_2[,i] * h_k[i]
+    }
+    
+    var_r <- rowSums(r_i_minus_r_2)
+  }
   
-  return(list(result = res_df,
-              iterations = apply(res[[3]],2,function(x) {1 - cumsum(x)}),
-              niter = niter))
+  # create final data frame
+  if (jackknife_var) {
+    res_df <- data.frame(t0 = res[[1]],
+                         t1 = res[[2]],
+                         est = 1 - cumsum(res[[3]][,niter]),
+                         var_est = var_r)
+    ret_lst <- list(result = res_df,
+                    iterations = apply(res[[3]],2,function(x) {1 - cumsum(x)}),
+                    niter = niter,
+                    niter_jackknife = niter_jackknife)
+  } else {
+    res_df <- data.frame(t0 = res[[1]],
+                         t1 = res[[2]],
+                         est = 1 - cumsum(res[[3]][,niter]))
+    ret_lst <- list(result = res_df,
+                    iterations = apply(res[[3]],2,function(x) {1 - cumsum(x)}),
+                    niter = niter)
+  }
+  
+  return(ret_lst)
 }
 
