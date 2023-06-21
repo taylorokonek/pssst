4a20ec4359b378456ccb77ae05fa359bf5467d4a
Taylor Okonek <tokonek@uw.edu>
2023-06-21 13:22:55 -0700

    remove print statements from debugging
    oops

--- src/rcpp_turnbull.cpp
+++ src/rcpp_turnbull.cpp
@@ -36,8 +36,6 @@ List rcpp_turnbull(int niter, NumericVector t0, NumericVector t1, NumericVector 
 	}
 	NumericVector temp2 = sort_unique(temp);
 
-	Rcout << "temp2: " << temp2 << "\n";
-
 	// declare q_j and p_j for lower and upper bounds of intervals
 	NumericVector q_j, p_j;
 
@@ -69,8 +67,6 @@ List rcpp_turnbull(int niter, NumericVector t0, NumericVector t1, NumericVector 
 		}
 	}
 
-	Rcout << "num_closed: " << num_closed << "\n";
-
 	NumericVector which_closed(num_closed);
 	if (num_closed > 0) {
 		// figure out which values are closed intervals
@@ -85,8 +81,6 @@ List rcpp_turnbull(int niter, NumericVector t0, NumericVector t1, NumericVector 
 	
 	// get unique, exactly observed observations
 	NumericVector which_closed_unique = sort_unique(which_closed);
-
-	Rcout << "which_closed_unique: " << which_closed_unique << "\n";
 
 	// update q_j, p_j to include which_closed
 	NumericVector q_j_final, p_j_final;
@@ -112,12 +106,6 @@ List rcpp_turnbull(int niter, NumericVector t0, NumericVector t1, NumericVector 
 
 	q_j_final = q_j_final.sort();
 	p_j_final = p_j_final.sort();
-
-	Rcout << "q_j: " << q_j << "\n";
-	Rcout << "q_j_final: " << q_j_final << "\n";
-
-	Rcout << "p_j: " << p_j << "\n";
-	Rcout << "p_j_final: " << p_j_final << "\n";
 
 	// make lists of indicators for each individual
 	// alpha_ij: indicator that the interval is a subset of their censoring set
